4416d0454a2c309ba7dd80f54d6cb762c17a37d8
GermanCoding <github@germancoding.com>
2022-07-26 20:33:27 +0200

    Visual improvements: Spinner, HTTP errors

--- web/js/submit.js
+++ web/js/submit.js
@@ -1,27 +1,43 @@
 function send_it(form) {
     let url = form.url.value;
-    $("#results").empty();
+    start();
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "backend/scanurl");
     xhr.setRequestHeader("Accept", "application/json");
     xhr.setRequestHeader("Content-Type", "application/json");
     let data = JSON.stringify({"url": url});
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // Print received data from server
-            let response = JSON.parse(this.responseText);
-            $.each(response['notices'], function (_, item) {
-                $("#results").append("<div class=\"alert alert-secondary my-2\">" + escapeHTML(item) + "</div>")
-            });
-            $.each(response['warnings'], function (_, item) {
-                $("#results").append("<div class=\"alert alert-warning my-2\">" + escapeHTML(item) + "</div>")
-            });
-            $.each(response['errors'], function (_, item) {
-                $("#results").append("<div class=\"alert alert-danger my-2\">" + escapeHTML(item) + "</div>")
-            });
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                // Print received data from server
+                let response = JSON.parse(this.responseText);
+                $.each(response['notices'], function (_, item) {
+                    $("#results").append("<div class=\"alert alert-secondary my-2\">" + escapeHTML(item) + "</div>")
+                });
+                $.each(response['warnings'], function (_, item) {
+                    $("#results").append("<div class=\"alert alert-warning my-2\">" + escapeHTML(item) + "</div>")
+                });
+                $.each(response['errors'], function (_, item) {
+                    $("#results").append("<div class=\"alert alert-danger my-2\">" + escapeHTML(item) + "</div>")
+                });
+            } else {
+                $("#results").append("<div class=\"alert alert-danger my-2\">Unable to communicate with server: HTTP error " + xhr.status + " " + xhr.statusText + "</div>")
+            }
+            end();
         }
     };
     xhr.send(data);
+}
+
+function start() {
+    $("#results").empty();
+    $("#submit").prop("disabled", true);
+    $("#spinner").prop("hidden", false);
+}
+
+function end() {
+    $("#submit").prop("disabled", false);
+    $("#spinner").prop("hidden", true);
 }
 
 function escapeHTML(unsafeText) {
